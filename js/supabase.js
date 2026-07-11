aeb9e4a49862905b206b988af9e9b12c8995bd0b
MR. SITTHICHAI SAENGCHOO <146951675+Chai147258@users.noreply.github.com>
2026-07-11 10:50:19 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://hkwqrllqzfzsbsxqgaoo.supabase.co";
 
-const SUPABASE_ANON_KEY = "ใส่ Anon Key ใหม่จาก Supabase";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhrd3FybGxxemZ6c2JzeHFnYW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNzIzNjAsImV4cCI6MjA5Njk0ODM2MH0.kPT_UY0-2-0cbDlGwya24DnOLJXA-XJGMdM6SIm7Rew";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
