51d0248b31d20f10f19d520db5d1841fe20742e8
MR. SITTHICHAI SAENGCHOO <146951675+Chai147258@users.noreply.github.com>
2026-07-16 13:20:09 +0700

    Remove unused mockup/template file: assets/js/js/supabase.js

--- assets/js/js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://hkwqrllqzfzsbsxqgaoo.supabase.co";
-
-const SUPABASE_ANON_KEY = "ใส่ Anon Key ของคุณ";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
