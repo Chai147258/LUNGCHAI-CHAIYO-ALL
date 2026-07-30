55f9d1a248b84fe01eee28c6ca062e862614624f
MR. SITTHICHAI SAENGCHOO <146951675+Chai147258@users.noreply.github.com>
2026-07-30 07:35:45 +0700

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>LUNGCHAI CHAIYO ALL</h1>
-      <p>One Platform. Everything Business.</p>
+      <h1>LUNGCHAI CHAIYO ALL TEST</h1>
     </main>
   );
 }
