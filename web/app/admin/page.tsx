1b68cec23ff6d61a410a80ff0a1a148ed9eebaeb
MR. SITTHICHAI SAENGCHOO <146951675+Chai147258@users.noreply.github.com>
2026-07-30 13:23:34 +0700

    Update page.tsx

--- web/app/admin/page.tsx
+++ web/app/admin/page.tsx
@@ -1,1 +1,12 @@
-
+export default function AdminPage() {
+  return (
+    <main>
+      <h1>
+        LUNGCHAI ADMIN
+      </h1>
+      <p>
+        Admin Route Working
+      </p>
+    </main>
+  );
+}
