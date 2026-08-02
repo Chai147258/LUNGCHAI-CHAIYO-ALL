45adafcf3602ab4dcc86aca1f8dd745c5ce7d94c
MR. SITTHICHAI SAENGCHOO <146951675+Chai147258@users.noreply.github.com>
2026-08-02 23:44:04 +0700

    feat: wire ConsentGate into root layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConsentGate from "./components/ConsentGate";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +38,10 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Noto+Sans+Thai:wght@400;500;600;700&display=swap"
         />
       </head>
-      <body className="min-h-full flex flex-col bg-black">{children}</body>
+      <body className="min-h-full flex flex-col bg-black">
+        {children}
+        <ConsentGate />
+      </body>
     </html>
   );
 }
